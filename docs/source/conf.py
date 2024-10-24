6688f23b2d79cd9b2ed446109b50a407b1ad1258
Yehe Liu <yxl448@case.edu>
2024-10-24 06:23:38 -0400

    pre-commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,5 @@
-# Configuration file for the Sphinx documentation builder.
-#
+"""Configuration file for the Sphinx documentation builder."""
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -31,13 +31,9 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 # Comment out or remove this line if you're not using any static files
 # html_static_path = ["_static"]
-
-
-
